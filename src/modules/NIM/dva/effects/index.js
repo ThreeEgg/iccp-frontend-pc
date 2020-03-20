608e958455d042ab229e730207f80dd11945acbd
翁鹏辉 <codecomeon@gmail.com>
2020-03-20 18:32:24 +0800

    修复effects问题

--- src/modules/NIM/dva/effects/index.js
+++ src/modules/NIM/dva/effects/index.js
@@ -1,61 +1,88 @@
 // Action 提交的是 mutation，而不是直接变更状态。
 // Action 可以包含任意异步操作。
-import cookie from '../../utils/cookie'
-import pageUtil from '../../utils/page'
+import cookie from '../../utils/cookie';
+import pageUtil from '../../utils/page';
 
 /* 导出actions方法 */
-import { showLoading, hideLoading, showFullscreenImg, hideFullscreenImg } from './widgetUi'
-import { initNimSDK } from './initNimSDK'
-import { initChatroomSDK, resetChatroomSDK } from './initChatroomSDK'
-import { updateBlack } from './blacks'
-import { updateFriend, addFriend, deleteFriend } from './friends'
-import { resetSearchResult, searchUsers, searchTeam } from './search'
-import { deleteSession, setCurrSession, resetCurrSession } from './session'
-import { sendMsg, sendFileMsg, sendMsgReceipt, sendRobotMsg, revocateMsg, updateLocalMsg, getHistoryMsgs, resetNoMoreHistoryMsgs, continueRobotMsg } from './msgs'
-import { markSysMsgRead, resetSysMsgs, deleteSysMsgs, markCustomSysMsgRead } from './sysMsgs'
-import { sendChatroomMsg, sendChatroomRobotMsg, sendChatroomFileMsg, getChatroomHistoryMsgs } from './chatroomMsgs'
-import { initChatroomInfos, getChatroomInfo, getChatroomMembers, clearChatroomMembers } from './chatroomInfos'
-import { delegateTeamFunction, onTeamNotificationMsg, enterSettingPage, getTeamMembers, checkTeamMsgReceipt, getTeamMsgReads } from './team'
+import { showLoading, hideLoading, showFullscreenImg, hideFullscreenImg } from './widgetUi';
+import { initNimSDK } from './initNimSDK';
+import { initChatroomSDK, resetChatroomSDK } from './initChatroomSDK';
+import { updateBlack } from './blacks';
+import { updateFriend, addFriend, deleteFriend } from './friends';
+import { resetSearchResult, searchUsers, searchTeam } from './search';
+import { deleteSession, setCurrSession, resetCurrSession } from './session';
+import {
+  sendMsg,
+  sendFileMsg,
+  sendMsgReceipt,
+  sendRobotMsg,
+  revocateMsg,
+  updateLocalMsg,
+  getHistoryMsgs,
+  resetNoMoreHistoryMsgs,
+  continueRobotMsg,
+} from './msgs';
+import { markSysMsgRead, resetSysMsgs, deleteSysMsgs, markCustomSysMsgRead } from './sysMsgs';
+import {
+  sendChatroomMsg,
+  sendChatroomRobotMsg,
+  sendChatroomFileMsg,
+  getChatroomHistoryMsgs,
+} from './chatroomMsgs';
+import {
+  initChatroomInfos,
+  getChatroomInfo,
+  getChatroomMembers,
+  clearChatroomMembers,
+} from './chatroomInfos';
+import {
+  delegateTeamFunction,
+  onTeamNotificationMsg,
+  enterSettingPage,
+  getTeamMembers,
+  checkTeamMsgReceipt,
+  getTeamMsgReads,
+} from './team';
 
-function connectNim({ payload: { nim, obj } }, { call }) {
-  let { force } = Object.assign({}, obj)
+async function connectNim({ payload: { nim, obj } }, { call }) {
+  let { force } = Object.assign({}, obj);
   // 操作为内容页刷新页面，此时无nim实例
   if (!nim || force) {
     let loginInfo = {
       uid: cookie.readCookie('uid'),
       sdktoken: cookie.readCookie('sdktoken'),
-    }
+    };
     if (!loginInfo.uid) {
       // 无cookie，直接跳转登录页
-      pageUtil.turnPage('无历史登录记录，请重新登录', 'login')
+      pageUtil.turnPage('无历史登录记录，请重新登录', 'login');
     } else {
       // 有cookie，重新登录
-      call('initNimSDK', { loginInfo })
+      call('initNimSDK', { loginInfo });
     }
   }
 }
 
-function connectChatroom(nim, call, obj) {
-  let { chatroomId } = Object.assign({}, obj)
+async function connectChatroom(nim, call, obj) {
+  let { chatroomId } = Object.assign({}, obj);
   if (nim) {
     call('showLoading');
     nim.getChatroomAddress({
       chatroomId,
       done: function getChatroomAddressDone(error, obj) {
         if (error) {
-          alert(error.message)
-          location.href = '#/room'
-          return
+          alert(error.message);
+          location.href = '#/room';
+          return;
         }
-        call('initChatroomSDK', { obj })
-      }
-    })
+        call('initChatroomSDK', { obj });
+      },
+    });
   }
 }
 
 export default {
   *updateRefreshState({ put }) {
-    yield put({ type: 'updateRefreshState'});
+    yield put({ type: 'updateRefreshState' });
   },
 
   // UI 及页面状态变更
@@ -68,28 +95,28 @@ export default {
   // 连接sdk请求，false表示强制重连
   *connect(obj, { call, select }) {
     console.log('ok');
-    // const nim = yield select(state => state.chat.nim);
-    // let { type } = Object.assign({}, obj)
-    // // type 可为 nim chatroom
-    // type = type || 'nim'
-    // switch (type) {
-    //   case 'nim':
-    //     yield call(connectNim, { payload: { nim, obj } }, { call })
-    //     break
-    //   case 'chatroom':
-    //     connectChatroom(nim, call, obj)
-    //     break
-    // }
+    const nim = yield select(state => state.chat.nim);
+    let { type } = Object.assign({}, obj);
+    // type 可为 nim chatroom
+    type = type || 'nim';
+    switch (type) {
+      case 'nim':
+        yield call(connectNim, { payload: { nim, obj } }, { call });
+        break;
+      case 'chatroom':
+        yield call(connectChatroom, nim, call, obj);
+        break;
+    }
   },
 
   // 用户触发的登出逻辑
   logout({ state, commit }) {
-    cookie.delCookie('uid')
-    cookie.delCookie('sdktoken')
+    cookie.delCookie('uid');
+    cookie.delCookie('sdktoken');
     if (state.nim) {
-      state.nim.disconnect()
+      state.nim.disconnect();
     }
-    pageUtil.turnPage('', 'login')
+    pageUtil.turnPage('', 'login');
   },
 
   // 初始化 重新连接SDK
@@ -140,7 +167,6 @@ export default {
   getChatroomMembers,
   clearChatroomMembers,
 
-
   // 搜索群
   searchTeam,
   // 代理sdk中的群方法
@@ -155,5 +181,4 @@ export default {
   checkTeamMsgReceipt,
   // 查询群消息回执已读列表
   getTeamMsgReads,
-
-}
+};
