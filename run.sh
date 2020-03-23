f00321093cc064574c88ac5d39b5fdce54b2a5ed
翁鹏辉 <codecomeon@gmail.com>
2020-03-23 17:07:46 +0800

    更新服务部署配置文件

--- run.sh
+++ run.sh
@@ -1,29 +1,2 @@
-# 参数的可选项
-# $1 --env
-# $2 test/online
-
-# 运行示例: sh run.sh --env test
-
-#! /bin/bash
-
-if [ "$1" == "--env" ]; then
-  if [ "$2" == "test" ]; then
-    # 端口
-    export PORT=8090
-    # 环境
-  # 线上
-  elif [ "$2" == "online" ]; then
-    echo "online"
-  else
-    echo "default"
-  fi
-fi
-
-#使用淘宝镜像安装依赖包
-npm install --registry=http://registry.npm.taobao.org
-
-# 编译
-npm run build
-
 # pm2运行
 # npm run pm2
