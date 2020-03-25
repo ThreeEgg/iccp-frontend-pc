1f891e9cf8b2ec4a2a04c3afb9f2329f345a790d
翁鹏辉 <codecomeon@gmail.com>
2020-03-25 18:49:01 +0800

    首页ssr

---
+++ src/common/enum.js
@@ -0,0 +1,9 @@
+export const platformContentType = {
+  PLATFORMINTRO: 'platformIntro', // 平台介绍
+  BUSINESSINTRO: 'businessIntro', // 业务介绍
+  CLASSICCASE: 'classicCase', // 经典案例
+  PARTNER: 'partner', // 合作伙伴
+  CLAUSE: 'clause', // 条款规定
+  COMMONQUESTION: 'commonQuestion', // 常见问题
+  REGISTRYAGGREEMENT: 'registryAggreement', // 注册协议
+};
