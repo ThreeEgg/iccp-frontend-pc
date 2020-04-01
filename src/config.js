f1d9ce144e0538dcff12929d91f595485caeebda
翁鹏辉 <codecomeon@gmail.com>
2020-04-01 15:25:20 +0800

    修复测试环境api跨域问题

--- src/config.js
+++ src/config.js
@@ -1,6 +1,24 @@
+const apiEnv = process.env.API_ENV;
+
+let baseUrl = 'http://221.215.57.110:9821';
+
+switch (apiEnv) {
+  case 'test':
+    baseUrl = 'http://172.16.1.161';
+    break;
+  case 'online':
+    baseUrl = 'http://221.215.57.110:9821';
+    break;
+  case 'dev':
+    baseUrl = 'http://221.215.57.110:9090';
+    break;
+  default:
+}
+
 export default {
-  baseUrl: 'http://221.215.57.110:9821', // 公网测试
-  // baseUrl: 'http://172.16.1.161:8080' // 内网测试
+  baseUrl,
+  // baseUrl: 'http://221.215.57.110:9821', // 公网测试
+  // baseUrl: 'http://172.16.1.161' // 内网测试
   // baseUrl: 'http://221.215.57.110:9090' // 公网开发
-  // baseUrl: 'http://172.16.220.5:9090' // 内网开发
+  // baseUrl: 'http://172.16.110.5:9090' // 内网开发
 };
