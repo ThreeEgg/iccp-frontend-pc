a0ee7fdd10c032fd1da6407ac4077203d8a1068d
翁鹏辉 <codecomeon@gmail.com>
2020-05-19 16:22:22 +0800

    v1.0 front-end 紧急修复域名

--- src/config.js
+++ src/config.js
@@ -9,14 +9,14 @@
  */
 const apiEnv = process.env.API_ENV;
 
-let baseUrl = 'https://iccp.wphkj.cn';
+let baseUrl = 'http://221.215.57.110:9821';
 
 switch (apiEnv) {
   case 'test':
     baseUrl = 'http://172.16.1.161';
     break;
   case 'online':
-    baseUrl = 'https://iccp.wphkj.cn';
+    baseUrl = 'http://221.215.57.110:9821';
     break;
   case 'dev':
     baseUrl = 'http://221.215.57.110:9090';
